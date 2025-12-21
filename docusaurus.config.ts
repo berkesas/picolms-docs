9256092c08d14c7fa50ea3ea588385f1ef8baf70
Nazar <19214701+berkesas@users.noreply.github.com>
2025-12-21 18:57:05 -0500

    fix repo name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://berkesas.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/picolms-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'berkesas', // Usually your GitHub org/user name.
-  projectName: 'picolms', // Usually your repo name.
+  projectName: 'picolms-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
